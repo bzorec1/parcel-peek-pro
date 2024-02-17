c59c7dfec8e4b14dbf279b7c96883f4b6670e7ff
bzorec <blaz.zorec2002@gmail.com>
2024-02-17 13:00:58 +0100

    added spinner

---
+++ web-app/src/ParcelPeekPro.WebApp/Components/Shared/Spinner/SpinnerService.cs
@@ -0,0 +1,11 @@
+﻿namespace ParcelPeekPro.WebApp.Components.Shared.Spinner;
+
+public class SpinnerService
+{
+    public event Action<bool> OnShow;
+    
+    public void ShowSpinner(bool show)
+    {
+        OnShow?.Invoke(show);
+    }
+}
